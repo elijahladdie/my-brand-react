4b4e6cc06330a2ae481b4d62de4113ab6f0e3702
elijah <elijahladdie2004@gmail.com>
2024-04-08 18:19:17 +0200

    fixing log out bug

--- src/component/protectedRoutes.jsx
+++ src/component/protectedRoutes.jsx
@@ -2,7 +2,6 @@ import { Outlet, Navigate } from "react-router-dom"
 
 const ProtectedRoute = () => {
     const token = localStorage.getItem('token');
-
     return token ? <Outlet /> : <Navigate to="/auth/login" />
 }
 
